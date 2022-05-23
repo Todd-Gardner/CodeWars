78eca5ace3a64d325f2e5f95454f94b1b191a204
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-05-23 14:25:33 -0400

    8-kyu: Remove first and last characters from a string.

--- 6-kyu/Your-order-please.js
+++ 6-kyu/Your-order-please.js
@@ -21,24 +21,23 @@ function order(words) {
     tempA[word.match(/[0-9]/g) - 1] = word;
   }
 
-  // ----------------------------------------------------
-
   // --- Time: 878ms ---
   //   words.split(" ").wordArr.map((word) => (tempA[word.match(/[0-9]/g) - 1] = word));
 
-  // Others answers:
-  // function order(words) {
-  //   return words
-  //     .split(" ")
-  //     .sort(function (a, b) {
-  //       return a.match(/\d/) - b.match(/\d/);
-  //     })
-  //     .join(" ");
-  // }
-
   return tempA.join(" ");
 }
+// Others answers:
+// function order(words) {
+//   return words
+//     .split(" ")
+//     .sort(function (a, b) {
+//       return a.match(/\d/) - b.match(/\d/);
+//     })
+//     .join(" ");
+// }
 
+
+// --- Tests ---
 // const { assert } = require("chai");
 
 // describe("order", () => {
---
+++ 8-kyu/Remove-First-and-Last-Character.js
@@ -0,0 +1,28 @@
+/* *** Instructions ***
+
+ It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.*/
+
+function removeChar(str) {
+  // --- Time: 897ms ---
+  return str.slice(1, str.length - 1);
+}
+// Other answers:
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
+
+
+// --- Tests ---
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Tests", () => {
+//   it("Fixed Tests", () => {
+//     assert.strictEqual(removeChar("eloquent"), "loquen");
+//     assert.strictEqual(removeChar("country"), "ountr");
+//     assert.strictEqual(removeChar("person"), "erso");
+//     assert.strictEqual(removeChar("place"), "lac");
+//     assert.strictEqual(removeChar("ooopsss"), "oopss");
+//   });
+// });
