e2f19a3c14afdf7a62238672349655f72bde545a
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-05-26 16:22:08 -0400

    Added tests and other answers for Are they the same

--- 6-kyu/Are-they-the-same.js
+++ 6-kyu/Are-they-the-same.js
@@ -35,3 +35,28 @@ function comp(array1, array2) {
   // --- Time: 714ms ---
   //   return array2.every((el, idx) => el == array1[idx]**2);
 }
+// Others answers:
+// function comp(array1, array2) {
+//   if (array1 == null || array2 == null) return false;
+//   array1.sort((a, b) => a - b);
+//   array2.sort((a, b) => a - b);
+//   return array1.map((v) => v * v).every((v, i) => v == array2[i]);
+// }
+
+// --- Tests ---
+// describe("Tests", () => {
+//   it("test", () => {
+//     a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+//     a2 = [
+//       11 * 11,
+//       121 * 121,
+//       144 * 144,
+//       19 * 19,
+//       161 * 161,
+//       19 * 19,
+//       144 * 144,
+//       19 * 19,
+//     ];
+//     Test.assertEquals(comp(a1, a2), true);
+//   });
+// });
