e178a37b3880dbc1d8c0b62636b0dce80cb65838
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-06-04 13:01:10 -0400

    8KYU - Grasshopper - Summation. Look at formula in other

---
+++ 8-kyu/Grasshopper-Summation.js
@@ -0,0 +1,33 @@
+/* *** Instructions *** 
+Summation
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+FUNDAMENTALS */
+
+var summation = function (num) {
+    // --- Time: 639ms ---
+  let total = 0;
+  for (let i = 0; i <= num; i++) {
+    total += i;
+  }
+    return total;
+    
+    // Other answers:
+    // return (num * (num + 1)) / 2;
+};
+
+// --- Tests ---
+// describe("summation", function () {
+//   it("should return the correct total", function () {
+//     Test.assertEquals(summation(1), 1);
+//     Test.assertEquals(summation(8), 36);
+//   });
+// });
