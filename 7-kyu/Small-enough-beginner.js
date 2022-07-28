34118eca37b77d917ebc7bbd14cc1a25b320b775
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2022-07-28 13:00:08 -0400

    7KYU - Small enough? - Beginner (Arrays, fundamentals)

---
+++ 7-kyu/Small-enough-beginner.js
@@ -0,0 +1,34 @@
+/*  *** Instructions ***
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+
+FUNDAMENTALS ARRAYS */
+
+function smallEnough(a, limit) {
+  //   --- Time: 936ms ---
+  return a.every((v) => v <= limit);
+
+  // Other answers:
+  // return Math.max(...a) <= limit;
+}
+
+// --- Tests ---
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(smallEnough([66, 101], 200), true);
+//     assert.strictEqual(
+//       smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100),
+//       false
+//     );
+//     assert.strictEqual(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
+//     assert.strictEqual(
+//       smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120),
+//       true
+//     );
+//   });
+// });
