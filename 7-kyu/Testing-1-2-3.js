0db8673ad4d1a7ca996875a62a95819dbddc07fa
Todd-Gardner <Todd-Gardner@users.noreply.github.com>
2023-04-20 14:04:33 -0400

    7kyu - Testing 1-2-3 (ARRAYS FUNDAMENTALS)

---
+++ 7-kyu/Testing-1-2-3.js
@@ -0,0 +1,33 @@
+/* *** Instructions ***
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+ARRAYS FUNDAMENTAL */
+
+var number = function (array) {
+  // --- Time: 850ms ---
+  return array.length > 0 ? array.map((el, idx) => `${idx + 1}: ${el}`) : array;
+};
+
+// --- Tests ---
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(number([]), [], "Empty array should return empty array");
+//     assert.deepEqual(
+//       number(["a", "b", "c"]),
+//       ["1: a", "2: b", "3: c"],
+//       "Return the correct line numbers"
+//     );
+//   });
+// });
